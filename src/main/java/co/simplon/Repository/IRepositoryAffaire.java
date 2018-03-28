cced916858216376a6429b779cb6c1800aafbbbf
nwardez <yann.colletlp2@gmail.com>
2018-03-28 10:23:07 +0200

    Commit du back CRUD Yann

---
+++ src/main/java/co/simplon/Repository/IRepositoryAffaire.java
@@ -0,0 +1,18 @@
+package co.simplon.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+
+
+import co.simplon.ModelEntity.AffaireModel;
+
+public interface IRepositoryAffaire  extends JpaRepository<AffaireModel, Long>{
+	
+	/*@Query("select a from AffaireModel a where a.nom like:x")
+	public List<AffaireModel> affaireParMc(@Param("x")String mc);*/
+
+}
