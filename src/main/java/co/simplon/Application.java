da74270ea37d922eff5e67858464a026a149a6b2
nwardez <yann.colletlp2@gmail.com>
2018-04-09 13:45:32 +0200

    Tests unitaire ok

--- src/main/java/co/simplon/Application.java
+++ src/main/java/co/simplon/Application.java
@@ -2,8 +2,15 @@ package co.simplon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+
+import com.example.demo.AffaireRepositoryTest;
+
 
 @SpringBootApplication
+@SpringBootTest(classes=AffaireRepositoryTest.class)
+@ContextConfiguration
 public class Application {
 
 	public static void main(String[] args) {
--- src/test/java/com/example/demo/AffaireRepositoryTest.java
+++ src/test/java/com/example/demo/AffaireRepositoryTest.java
@@ -1,16 +1,18 @@
-package co.simplon.Controller;
+package com.example.demo;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import co.simplon.Application;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT,
@@ -23,7 +25,7 @@ public class AffaireRepositoryTest {
 	MockMvc mockMvc;
 	
 	@Test
-	public void contextLoads() throws Exception {
+	public void retoursAffaires() throws Exception {
 		MvcResult mvcResult= mockMvc.perform(
 				MockMvcRequestBuilders.get("/affaires")
 				.accept(MediaType.APPLICATION_JSON)
